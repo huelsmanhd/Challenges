3784fe0ce94e2cb6634297ad1df223291fe1c82e
Henry Huelsman <hhuelsman14@gmail.com>
2019-01-10 15:49:47 -0500

    chal commit

--- challenge1.js
+++ challenge1.js
@@ -1,1 +1,19 @@
+function palindrome(str) {
+    let len = str.length;
+    let strp = len.split('').reverse().join('');
+    for (i = 0; i < strp; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            console.log('false');
+        }   else {
+            console.log('true')
+        }
+    }
+}
+palindrome('dad');
 
+/* What do I need?
+function for palindrome
+parameter for paindrome input
+methods for reversing ,splitting, and reentering string
+if statment to execute true/false if palindrome exists
+*/
